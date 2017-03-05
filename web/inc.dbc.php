1193202e48b768b7207a1f5d380f7a5a110fdd0c
Tyler Stinson <tjstinso@gmail.com>
2017-03-04 16:21:16 -0800

    better

--- web/inc.dbc.php
+++ web/inc.dbc.php
@@ -1,7 +1,7 @@
 <?php
 
 	function get_connection() {
-    $arr = parse_ini_file ('config.conf');
+    $arr = parse_ini_file ('../config.conf');
 		$dsn = 'mysql:host=cssgate.insttech.washington.edu;dbname=' . $arr[dbname]; //Change dbname to yours
 		$userid = $arr[userid]; //Change this to yours
 		$password = $arr[password]; //Change this to yours
